f1b6d2a99a9550ea47559f4427fec2d4e856c9a3
Jacob Lamb <44789941+jacobdalamb@users.noreply.github.com>
2025-11-09 13:42:43 -0800

    Format files

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,9 +5,9 @@ import { autoSidebarLoader } from "starlight-auto-sidebar/loader";
 import { autoSidebarSchema } from "starlight-auto-sidebar/schema";
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  autoSidebar: defineCollection({
-    loader: autoSidebarLoader(),
-    schema: autoSidebarSchema(),
-  }),
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	autoSidebar: defineCollection({
+		loader: autoSidebarLoader(),
+		schema: autoSidebarSchema(),
+	}),
 };
