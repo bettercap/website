d9107d81494acf64cd862eb1f683c3c49320960b
Simone Margaritelli <evilsocket@gmail.com>
2026-02-03 18:09:35 +0100

    minor improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightAutoSidebar from "starlight-auto-sidebar";
 import starlightFullViewMode from "starlight-fullview-mode";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bettercap.org",
   integrations: [
+    sitemap(),
     starlight({
       title: "bettercap",
       editLink: {
@@ -52,15 +54,35 @@ export default defineConfig({
         {
           tag: "script",
           attrs: {
-            src: "https://evilsocket.net/a.js",
+            type: "application/ld+json",
           },
-        },
-        {
-          tag: "script",
-          attrs: {
-            type: "text/javascript",
-          },
-          content: "A['\\u0069\\u006E\\u0069\\u0074']({ '\\u0061\\u0063\\u0074\\u0069\\u006F\\u006E': 'none', 'statsEndpoint': '\\u0068\\u0074\\u0074\\u0070\\u0073\\u003A\\u002F\\u002F\\u006E\\u006F\\u0062\\u006F\\u0074\\u002D\\u0073\\u0074\\u0061\\u0074\\u0073\\u002E\\u0065\\u0076\\u0069\\u006C\\u0073\\u006F\\u0063\\u006B\\u0065\\u0074\\u002E\\u0077\\u006F\\u0072\\u006B\\u0065\\u0072\\u0073\\u002E\\u0064\\u0065\\u0076\\u002F\\u006C\\u006F\\u0067' });",
+          content: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "SoftwareApplication",
+            "name": "BetterCAP",
+            "description": "The Swiss Army knife for WiFi, Bluetooth Low Energy, wireless HID hijacking, CAN-bus and IPv4/IPv6 network reconnaissance and MITM attacks.",
+            "applicationCategory": "SecurityApplication",
+            "operatingSystem": "Linux, macOS, Windows, Android",
+            "url": "https://www.bettercap.org",
+            "downloadUrl": "https://github.com/bettercap/bettercap/releases",
+            "softwareVersion": "2.x",
+            "offers": {
+              "@type": "Offer",
+              "price": "0",
+              "priceCurrency": "USD"
+            },
+            "author": {
+              "@type": "Person",
+              "name": "Simone Margaritelli",
+              "url": "https://www.evilsocket.net",
+              "sameAs": [
+                "https://github.com/evilsocket",
+                "https://twitter.com/evilsocket"
+              ]
+            },
+            "codeRepository": "https://github.com/bettercap/bettercap",
+            "programmingLanguage": "Go"
+          }),
         },
       ],
     }),
